73388e73caaa6a5b749df784030977a5f5f4ec98
Shubham Patel <ptlshubham@gmail.com>
2023-06-14 15:01:04 +0530

    all changes

--- src/app/basic/basic.module.ts
+++ src/app/basic/basic.module.ts
@@ -9,6 +9,7 @@ import { ActivityComponent } from './activity/activity.component';
 import { ContactComponent } from './contact/contact.component';
 import { StructureComponent } from './structure/structure.component';
 import { AboutComponent } from './about/about.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -25,6 +26,8 @@ import { AboutComponent } from './about/about.component';
     SharedModule,
     RouterModule.forChild(BasicRoutes),
     NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class BasicModule { }
